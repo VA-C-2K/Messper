cf7c1bec6b7e4f346c9b1087a4c2932d2ac150b3
Viraj-creation312 <virajjerry123@gmail.com>
2022-05-06 13:12:01 +0530

    Changes made in login/register page

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,15 +2,8 @@ import React from "react";
 import'./css/Loading.css'
 const Loading = () => {
   return (
-    <div style={{ position: "relative" }}>
-    <h2
-       style={{
-         position: "fixed",
-         top: "50%",
-         left: "50%",
-         transform: "translate(-50%, -50%)",
-        }}
-      > 
+    <div className="loadingDiv">
+    <h2> 
        <div className="spinner"></div>
       </h2>
    </div>
